b8a4548acc76c5eeddebacfc2e74324346589b92
hoanghai0827 <honghi.hunh@gmail.com>
2022-12-04 16:43:03 +0700

    video #12

---
+++ bai6/video12.js
@@ -0,0 +1,11 @@
+console.log(`Xin chào world`)
+let personal = {
+    name: 'Hoang Hai',
+    age: '19',
+    school: 'graduted',
+    university: 'STU',
+    relationship: false,
+};
+console.log("Tên tôi là : ", personal.name);
+console.log("Tuổi : ", personal.age);
+console.log("Trường học: ", personal.school)
