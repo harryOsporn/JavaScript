c6b8f23d0ecbfff4e84f8f7f08d23918c70c0453
hoanghai0827 <honghi.hunh@gmail.com>
2022-12-04 19:18:08 +0700

    Array_in_JS

---
+++ video13/video13.js
@@ -0,0 +1,33 @@
+let myObj = {
+    name: "Hoang hải",
+    age: 19,
+    school: "STU"
+};
+console.log("Type of myCurrent_Object: ", myObj);
+
+let arrayFootballer = [
+    "Messi",
+    "Ronaldo",
+    "Neymar"];
+console.log("Type current_array: ", arrayFootballer);
+console.log("First array: ", arrayFootballer[0]);
+//Array + object
+let myArray_Object = [
+    {
+        name: "Hoàng Hải",
+        age: 19,
+        job: false
+    },
+    {
+        name: "vợ Hoàng Hải",
+        age: 60,
+        job: "sugar mommy"
+    },
+    {
+        name: "vợ bé",
+        age: 17,
+        job: "Hai second wife"
+    }
+];
+console.log("Object in array: ", myArray_Object);
+console.log("Third object in array: ", myArray_Object[2]);
