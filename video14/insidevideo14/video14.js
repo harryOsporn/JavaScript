7172906e0dd812f4cc4fa77d918e01e71a9037a1
hoanghai0827 <honghi.hunh@gmail.com>
2022-12-05 18:33:23 +0700

    video14 : compare == and ===

---
+++ video14/insidevideo14/video14.js
@@ -0,0 +1,12 @@
+console.log(`Hello world`)
+// === is compare type of two value . For instance
+let a = 'Hoàng Hải';
+let b = {
+    name: 'Hoang Hai',
+    age: 19,
+    uni: 'STU',
+};
+console.log("Ta có : a là string, b là obj")
+console.log("a == b : ", a == b);
+console.log("a === b : ", a === b);
+console.log("a !== b : ", a !== b);
